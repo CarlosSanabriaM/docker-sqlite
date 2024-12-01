55576a995f5a4a34390f012cad645ca1e4c027af
CarlosSanabriaM <sanabria.miranda.carlos@gmail.com>
2024-12-01 14:40:07 +0100

    feat: add conditional database recreation and initialization based on environment variable

--- database/scripts/initialize-database.sh
+++ database/scripts/initialize-database.sh
@@ -3,16 +3,22 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "💭 Initializing database..."
+# Only run the SQL scripts if the environment variable is set to true
+if [ "$RECREATE_DATABASE_WITH_INITIAL_TEST_DATA" = "true" ]; then
+    echo "💭 Recreating and initializing the database..."
 
-# Define the database file
-DATABASE_FILE="/database/data/database.db"
-
-# Execute the SQL scripts over the database
-sqlite3 $DATABASE_FILE < /database/scripts/ddl/pedidos-create.sql
-sqlite3 $DATABASE_FILE < /database/scripts/dml/pedidos-insert.sql
-
-echo "✅ Database initialized successfully!"
+    # Define the database file
+    DATABASE_FILE="/database/data/database.db"
+  
+    # Execute the SQL scripts over the database
+    sqlite3 $DATABASE_FILE < /database/scripts/ddl/pedidos-create.sql
+    sqlite3 $DATABASE_FILE < /database/scripts/dml/pedidos-insert.sql
+  
+    echo "✅ Database recreated and initialized successfully"
+else
+    echo "⏩ Skipped execution of scripts that recreate and initialize the database"
+fi
 
 # Keep the connection to the database alive
+echo "🚀 Database container is running..."
 tail -f /dev/null
